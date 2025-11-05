beb020de6bc3c1a4a966b7078c5682170017b78c
ddingjoo <ddingsha9@teambind.co.kr>
2025-11-05 13:32:56 +0900

    [TEST] 테스트용 Redis 설정을 위한 TestRedisConfig 추가 springProject/src/test/java/com/teambind/springproject/config/TestRedisConfig.java: ShedLockConfig 테스트를 위한 Mock RedisConnectionFactory 제공
    참고: Redis 테스트 관련 추가 검토 필요
    
    Signed-off-by: ddingjoo <ddingsha9@teambind.co.kr>

---
+++ springProject/src/test/java/com/teambind/springproject/config/TestRedisConfig.java
@@ -0,0 +1,23 @@
+package com.teambind.springproject.config;
+
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+
+import static org.mockito.Mockito.mock;
+
+/**
+ * 테스트용 Redis 설정.
+ *
+ * <p>ShedLockConfig가 요구하는 RedisConnectionFactory를 Mock으로 제공한다.
+ */
+@TestConfiguration
+public class TestRedisConfig {
+	
+	@Bean
+	@Primary
+	public RedisConnectionFactory redisConnectionFactory() {
+		return mock(RedisConnectionFactory.class);
+	}
+}
